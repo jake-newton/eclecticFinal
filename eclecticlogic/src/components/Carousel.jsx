9f56b87c703c8ea1799ba7019f36e1910f45c336
Connor Sullivan <crsullivan@protonmail.com>
2020-09-26 22:14:37 -0700

    updates caarousel

--- eclecticlogic/src/components/Carousel.jsx
+++ eclecticlogic/src/components/Carousel.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import AwesomeSlider from 'react-awesome-slider';
+import AutoplaySlider from 'react-awesome-slider';
 import 'react-awesome-slider/dist/styles.css';
 import "../App.css"
 
 const Carousel = () => (
-    <AwesomeSlider className="carousel"  bullets={false} autoplay={true}>
+    <AutoplaySlider 
+    className="carousel"  
+    bullets={false}     
+    play={true}
+    cancelOnInteraction={false}
+    interval={10}>
         <div data-src={require("../images/E_lecticLogi_ Logo (4).png")}></div>
         <div data-src={require("../images/covidtracker.PNG")}></div>
         <div data-src={require("../images/kPortfolio.PNG")}></div>
         <div data-src={require("../images/portfolioSnap.PNG")}></div>
         <div data-src={require("../images/updateddbArch.PNG")}></div>
         <div data-src={require("../images/portfolioSnip2.PNG")}></div>
-    </AwesomeSlider>
+    </AutoplaySlider>
   )
   
 export default Carousel
