5bcbb21ea62afe5a6656e1060454bae728d6c9bd
Connor Sullivan <crsullivan@protonmail.com>
2020-09-28 14:35:06 -0700

    updates image alt tags

--- eclecticlogic/src/components/Carousel.jsx
+++ eclecticlogic/src/components/Carousel.jsx
@@ -10,12 +10,12 @@ const Carousel = () => (
     play={true}
     cancelOnInteraction={false}
     interval={10}>
-        <div data-src={require("../images/E_lecticLogi_ Logo (4).png")}></div>
-        <div data-src={require("../images/covidtracker.PNG")}></div>
-        <div data-src={require("../images/kPortfolio.PNG")}></div>
-        <div data-src={require("../images/portfolioSnap.PNG")}></div>
-        <div data-src={require("../images/updateddbArch.PNG")}></div>
-        <div data-src={require("../images/portfolioSnip2.PNG")}></div>
+        <div alt="Company logo" data-src={require("../images/E_lecticLogi_ Logo (4).png")}></div>
+        <div alt="Covid-19 tracking project" data-src={require("../images/covidtracker.PNG")}></div>
+        <div alt="example portfolio one" data-src={require("../images/kPortfolio.PNG")}></div>
+        <div alt="example portfolio two" data-src={require("../images/portfolioSnap.PNG")}></div>
+        <div alt="example database architecture" data-src={require("../images/updateddbArch.PNG")}></div>
+        <div alt="example portfolio three" data-src={require("../images/portfolioSnip2.PNG")}></div>
     </AwesomeSlider>
   )
   
--- eclecticlogic/src/components/Footer.jsx
+++ eclecticlogic/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import "../App.css"
 const Footer = () => (
   <footer>
-    <div className="footer-img-div"><img src={require("../images/E_lecticLogi_ Logo (5).png")}></img></div>
+    <div className="footer-img-div"><img alt="Company logo" src={require("../images/E_lecticLogi_ Logo (5).png")}></img></div>
     <div className="copyright">© {new Date().getFullYear()} EclecticLogic</div>
     <div className="footer-links">
         <a class="fab fa-instagram fa-2x" target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/eclecticlogic/?hl=en"></a>
