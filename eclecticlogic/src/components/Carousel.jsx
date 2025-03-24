bd650464a6ee2a46816e24ab30fce7b8f2af16c8
Jake Newton <jake.newt@0dark30.com>
2025-03-23 17:19:38 -0700

    Updated links, removed carousel items.

--- eclecticlogic/src/components/Carousel.jsx
+++ eclecticlogic/src/components/Carousel.jsx
@@ -13,10 +13,7 @@ const Carousel = () => (
     >
         <div alt="Company logo" data-src={require("../images/E_lecticLogi_ Logo (4).png")}></div>
         <div alt="Covid-19 tracking project" data-src={require("../images/covidtracker.PNG")}></div>
-        <div alt="example portfolio one" data-src={require("../images/kPortfolio.PNG")}></div>
-        <div alt="example portfolio two" data-src={require("../images/portfolioSnap.PNG")}></div>
         <div alt="example database architecture" data-src={require("../images/updateddbArch.PNG")}></div>
-        <div alt="example portfolio three" data-src={require("../images/portfolioSnip2.PNG")}></div>
     </AwesomeSlider>
   )
   
